6eb63803ae3123c20e369434f9ecd75603b30fd4
Carlos Cespedes <cespedes88@gmail.com>
2023-12-14 20:31:08 -0800

    fetch fix

--- netlify/functions/fetchHF/fetchHF.js
+++ netlify/functions/fetchHF/fetchHF.js
@@ -5,9 +5,18 @@ const hf = new HfInference(process.env.HF_TOKEN || "")
 const handler = async (event) => {
   try {
     const data = JSON.parse(event.body)
+    console.log(data.text)
+    const response = await hf.translation({
+      model: "facebook/mbart-large-50-many-to-many-mmt",
+      inputs: data.text,
+      parameters: {
+        src_lang: "en_XX",
+        tgt_lang: data.language,
+      },
+    })
     return {
       statusCode: 200,
-      body: JSON.stringify(data.text),
+      body: JSON.stringify(response),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
