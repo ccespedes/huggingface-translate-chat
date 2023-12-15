96b80ac4d54af4453f0f94557f8ca44273f4ee13
Carlos Cespedes <cespedes88@gmail.com>
2023-12-14 20:07:43 -0800

    test return

--- netlify/functions/fetchHF/fetchHF.js
+++ netlify/functions/fetchHF/fetchHF.js
@@ -3,25 +3,28 @@ import { HfInference } from "@huggingface/inference"
 const hf = new HfInference(process.env.HF_TOKEN || "")
 
 const handler = async (event) => {
-  const data = JSON.parse(event.body)
-  return JSON.stringify(data)
-  // try {
-  //   const response = await hf.translation({
-  //     model: "facebook/mbart-large-50-many-to-many-mmt",
-  //     inputs: text,
-  //     parameters: {
-  //       src_lang: "en_XX",
-  //       tgt_lang: language,
-  //     },
-  //   })
+  try {
+    //   const response = await hf.translation({
+    //     model: "facebook/mbart-large-50-many-to-many-mmt",
+    //     inputs: text,
+    //     parameters: {
+    //       src_lang: "en_XX",
+    //       tgt_lang: language,
+    //     },
+    // })
+    const data = JSON.parse(event.body)
 
-  //   return {
-  //     statusCode: 200,
-  //     body: JSON.stringify({ message: `Hello ${subject}` }),
-  //   }
-  // } catch (error) {
-  //   return { statusCode: 500, body: error.toString() }
-  // }
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    }
+    //   return {
+    //     statusCode: 200,
+    //     body: JSON.stringify({ message: `Hello ${subject}` }),
+    //   }
+  } catch (error) {
+    //   return { statusCode: 500, body: error.toString() }
+  }
 }
 
 module.exports = { handler }
